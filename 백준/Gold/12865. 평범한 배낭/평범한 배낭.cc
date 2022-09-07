ba46e9998bbe0419b2526be9a59d9d976809d45e
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2022-09-08 04:37:40 +0900

    [Gold V] Title: 평범한 배낭, Time: 36 ms, Memory: 41476 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int N, K;
+int dp[101][100001];
+int W[101];
+int V[101];
+void input() {
+	cin >> N >> K;
+	for (int i = 1; i <= N; i++) {
+		cin >> W[i] >> V[i];
+	}
+}
+
+
+int main() {
+	input();
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= K; j++) {
+			if (j - W[i] >= 0) dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]);
+			else dp[i][j] = dp[i - 1][j];
+		}
+	}
+
+	cout << dp[N][K];
+	return 0;
+}
