56b91d46d30a111bfcd970b93d479124963591dd
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2022-10-18 13:48:44 +0900

    [level 2] Title: [1차] 캐시, Time: 0.40 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int cacheSize, vector<string> cities) {
+    int answer = 0;
+    vector<string> cache;
+    
+    if(cacheSize == 0) return 5 * cities.size();
+    
+    for(int i = 0; i < cities.size(); i++){
+        transform(cities[i].begin(), cities[i].end(), cities[i].begin(), ::tolower);
+        auto itr = find(cache.begin(), cache.end(), cities[i]);
+        
+        if(itr == cache.end()){
+            if(cache.size() == cacheSize) cache.erase(cache.begin());
+            answer += 5;
+        }
+        else{
+            answer += 1;
+            cache.erase(itr);
+        }
+        cache.push_back(cities[i]);
+    }
+    return answer;
+}
