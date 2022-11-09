1ff5edfc2f7de5c1fa3b62a07a68a19b90c48efd
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2022-11-09 17:09:43 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 84 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.cc
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.cc
@@ -13,19 +13,20 @@ void solve() {
 	dp[3][1] = 1;
 	dp[3][2] = 1;
 	dp[3][3] = 1;
-	for (int i = 4; i <= n; i++) {
+	for (int i = 4; i <= 100000; i++) {
 		dp[i][1] = (dp[i - 1][2] + dp[i - 1][3]) % mod;
 		dp[i][2] = (dp[i - 2][1] + dp[i - 2][3]) % mod;
 		dp[i][3] = (dp[i - 3][1] + dp[i - 3][2]) % mod;
 	}
-	cout << ((long long)dp[n][1] + dp[n][2] + dp[n][3]) % mod << "\n";
+	
 }
 
 int main() {
 	cin >> T;
+	solve();
 	while (T--) {
 		cin >> n;
-		solve();
+		cout << ((long long)dp[n][1] + dp[n][2] + dp[n][3]) % mod << "\n";
 	}
 	return 0;
 }
