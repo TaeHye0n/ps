443add3adb365293cbab74a6920e913d01376c43
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2022-11-10 17:42:02 +0900

    [Silver I] Title: 오르막 수, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+const int mod = 1e4 + 7;
+int N;
+int dp[1001][11];
+
+int main() {
+	cin >> N;
+	for (int i = 0; i <= 9; i++) {
+		dp[1][i] = 1;
+	}
+
+	for (int i = 2; i <= N; i++) {
+		for (int j = 0; j <= 9; j++) {
+			for (int k = j; k <= 9; k++) {
+				dp[i][j] += (dp[i - 1][k]) % mod;
+			}
+		}
+	}
+	int ans = 0;
+	for (int i = 0; i <= 9; i++) {
+		ans = (ans + dp[N][i]) % mod;
+	}
+	cout << ans;
+	return 0;
+}
