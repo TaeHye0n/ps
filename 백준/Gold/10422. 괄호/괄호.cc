f379d5d0d750e8e1c144c53db36ff4dd690ec956
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2022-11-18 17:10:31 +0900

    [Gold IV] Title: 괄호, Time: 8 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/10422. 괄호/괄호.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+typedef unsigned long long ull;
+const int mod = 1e9 + 7;
+ull dp[5001];
+
+void solve() {
+	dp[0] = 1;
+	dp[1] = 0;
+	dp[2] = 1;
+
+	for (int i = 3; i < 5001; i++) {
+		for (int j = 2; j <= i; j += 2) {
+			dp[i] += (dp[j - 2] * dp[i - j]) % mod;
+		}
+		dp[i] %= mod;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int T;
+	cin >> T;
+	solve();
+	while (T--) {
+		int L;
+		cin >> L;
+
+		if (L % 2 != 0) cout << 0 << "\n";
+		else cout << dp[L] << "\n";
+	}
+	return 0;
+}
