e580de24265a704e0346cb7e68678e9fca3701f5
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2023-03-05 19:21:09 +0900

    [unrated] Title: 숫자 변환하기, Time: 2.14 ms, Memory: 7.39 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.cpp
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.cpp
@@ -3,26 +3,26 @@
 #include <queue>
 using namespace std;
 
-int dist[1000001];
+int dp[1000001];
 
 int solution(int x, int y, int n) {
     int answer = 0;
     queue<int> q;
     q.push(x);
     for(int i = 0; i < 1000001; i++){
-        dist[i] = -1;
+        dp[i] = -1;
     }
-    dist[x] = 0;
-    
+    dp[x] = 0;
+
     while(!q.empty()){
         int curr = q.front();
         q.pop();
         for(int next : {curr + n, curr * 2, curr * 3}){
-            if(next < 0 || next > y) continue;
-            if(dist[next] != -1) continue;
-            dist[next] = dist[curr]+1;
+            if(next > y) continue;
+            if(dp[next] != -1) continue;
+            dp[next] = dp[curr] + 1;
             q.push(next);
         }
     }
-    return dist[y];
+    return dp[y];
 }
