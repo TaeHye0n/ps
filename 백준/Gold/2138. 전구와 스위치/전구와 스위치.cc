1d36698431a1a5127586ec86e408d5ac02c3b902
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2023-04-12 16:57:59 +0900

    [Gold V] Title: 전구와 스위치, Time: 0 ms, Memory: 2392 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.cc
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.cc
@@ -1,44 +1,53 @@
 #include <iostream>
 #include <string>
 #include <climits>
-
 using namespace std;
 
-string st, ed;
-string temp;
 int N;
 int ans = INT_MAX;
+string st, ed;
+string temp;
 
-void input() {
-	cin >> N;
-	cin >> st >> ed;
-}
-void lightChange(int idx) {
-	if (idx > 0) temp[idx - 1] = (temp[idx - 1] == '0') ? '1' : '0';
-	temp[idx] = (temp[idx] == '0') ? '1' : '0';
-	if (idx < N - 1) temp[idx + 1] = (temp[idx + 1] == '0') ? '1' : '0';
-}
-void solve(int start) {
+
+void solve() {
 	temp = st;
 	int cnt = 0;
-	if (start == 0) {
-		lightChange(0);
-		cnt++;
-	}
+	temp[0] = (temp[0] == '0') ? '1' : '0';
+	temp[1] = (temp[1] == '0') ? '1' : '0';
+	cnt++;
 	for (int i = 1; i < N; i++) {
 		if (temp[i - 1] != ed[i - 1]) {
-			lightChange(i);
 			cnt++;
+			temp[i - 1] = (temp[i - 1] == '0') ? '1' : '0';
+			temp[i] = (temp[i] == '0') ? '1' : '0';
+			if (i + 1 >= N) continue;
+			temp[i + 1] = (temp[i + 1] == '0') ? '1' : '0';
+		}
+	}
+	if (temp == ed) ans = min(ans, cnt);
+
+	temp = st;
+	cnt = 0;
+	for (int i = 1; i < N; i++) {
+		if (temp[i - 1] != ed[i - 1]) {
+			cnt++;
+			temp[i - 1] = (temp[i - 1] == '0') ? '1' : '0';
+			temp[i] = (temp[i] == '0') ? '1' : '0';
+			if (i + 1 >= N) continue;
+			temp[i + 1] = (temp[i + 1] == '0') ? '1' : '0';
 		}
 	}
 	if (temp == ed) ans = min(ans, cnt);
 }
-
 int main() {
-	input();
-	solve(0);
-	solve(1);
-	if (ans == INT_MAX) cout << -1;
-	else cout << ans;
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	
+	cin >> N;
+	cin >> st >> ed;
+	solve();
+	if (ans == INT_MAX) ans = -1;
+	cout << ans;
 	return 0;
 }
