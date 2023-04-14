c76d1d2ea467a4f533a59bc04d4086746d6c662a
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2023-04-14 21:54:05 +0900

    [level 3] Title: 순위, Time: 1.11 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/49191. 순위/순위.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+bool graph[101][101];
+int solution(int n, vector<vector<int>> results) {
+    int answer = 0;
+    
+    for(vector<int> v : results){
+        graph[v[0]][v[1]] = true;
+    }
+    
+    for(int k = 1; k <= n; k++){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(graph[i][k] && graph[k][j]) graph[i][j] = true;
+            }
+        }
+    }
+    
+    for(int i = 1; i <= n; i++){
+        int cnt = 0;
+        for(int j = 1; j <= n; j++){
+            if(graph[i][j] || graph[j][i]) cnt++;
+        }
+        if(cnt == n-1) answer++;
+    }
+    return answer;
+}
