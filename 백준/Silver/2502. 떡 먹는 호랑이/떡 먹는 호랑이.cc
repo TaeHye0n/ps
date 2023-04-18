504a0e9d5e8833b787e2fa3e7710c765a2bc6edb
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2023-04-18 16:30:40 +0900

    [Silver I] Title: 떡 먹는 호랑이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.cc
+++ 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.cc
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
 
+#define MAX 31
 using namespace std;
 
 int D; // 넘어온 날
 int K; // D날 준 떡의 개수
 
-int dp[31];
-
+int x[MAX];
+int y[MAX];
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -15,24 +16,24 @@ int main() {
 	cout.tie(nullptr);
 	
 	cin >> D >> K;
-	dp[D] = K;
+
+	x[1] = 1;
+	x[2] = 0;
+	y[1] = 0;
+	y[2] = 1;
+
+	for (int i = 3; i <= D; i++) {
+		x[i] = x[i - 1] + x[i - 2];
+		y[i] = y[i - 1] + y[i - 2];
+	}
 
 	for (int i = 1; i <= K; i++) {
-		bool flag = false;
-		dp[1] = i;
-		for (int j = i + 1; j <= K; j++) {
-			dp[2] = j;
-			for (int h = 3; h <= D; h++) {
-				dp[h] = dp[h - 1] + dp[h - 2];
-			}
-			if (dp[D] == K) {
-				flag = true;
-				break;
-			}
+		int Y = K - (x[D] * i);
+		if (Y % y[D] == 0) {
+			cout << i << "\n";
+			cout << Y / y[D];
+			break;
 		}
-		if (flag) break;
 	}
-	cout << dp[1] << "\n";
-	cout << dp[2];
 	return 0;
 }
