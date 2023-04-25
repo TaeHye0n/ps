52e57204977a8fa0ebca794283307af9dd3fc6e8
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2023-04-25 19:32:47 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.cpp
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.cpp
@@ -1,17 +1,30 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
+#include <iostream>
 using namespace std;
 
 int solution(int k, int m, vector<int> score) {
     int answer = 0;
-    sort(score.begin(),score.end(), greater<int>());
-    int idx = 0;
-    while(1){
-        if(idx+m-1 >= score.size()) break;
-        answer += score[idx+m-1]*m;
-        idx += m;
+    vector<int> box;
+    sort(score.begin(), score.end());
+    int idx = score.size()-1;
+    while(1)
+    {
+        if(idx < 0){
+            break;
+        }
+        if(box.size() < m)
+        {
+                box.push_back(score[idx]);
+                idx--;
+        }
+        
+        if(box.size() == m)
+        {
+            answer += box.back() * box.size();
+            box.clear();
+        }
     }
     return answer;
 }
