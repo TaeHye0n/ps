6b88e1b5c4fbc304d96c41b9e73b9ba87e555c96
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2023-07-31 15:44:08 +0900

    [Silver III] Title: N과 M (8), Time: 172 ms, Memory: 18740 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] nums;
+    static boolean[] used;
+    static StringBuilder sb = new StringBuilder();
+    static List<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        nums = new int[N];
+        used = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+        func(0, 0);
+        System.out.println(sb);
+    }
+
+    private static void func(int cnt, int idx) {
+        if (cnt == M) {
+            list.forEach(o -> sb.append(o).append(" "));
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = idx; i < N; i++) {
+            list.add(nums[i]);
+            func(cnt + 1, i);
+            list.remove(list.size() - 1);
+        }
+    }
+}
