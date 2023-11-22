4a159220d06829588ca57d18026e632c92b91fad
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2023-11-22 18:13:51 +0900

    [level 2] Title: 타겟 넘버, Time: 0.39 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,23 @@
+class Solution {
+    static int answer;
+    
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+        
+        dfs(numbers, target, -numbers[0], 0);
+        dfs(numbers, target, numbers[0], 0);
+        return answer;
+    }
+    
+    private void dfs(int[] numbers, int target, int sum, int depth) {
+        if (depth == numbers.length - 1) {
+            if (sum == target) {
+                answer++;
+            }
+            return;
+        }
+        
+        dfs(numbers, target, sum - numbers[depth + 1], depth + 1);
+        dfs(numbers, target, sum + numbers[depth + 1], depth + 1);
+    }
+}
