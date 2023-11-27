dcc4904361648c9ca6b980693b5da47bf2d49e8d
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2023-11-27 14:30:20 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.04 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,10 +1,12 @@
 class Solution {
+    static final char MAX_DIGIT = '9';
+    
     public String solution(String number, int k) {
         StringBuilder sb = new StringBuilder(number);
         
         int idx = -1;
-        for (int i = 0; i + 1 < sb.length() && k > 0; i++) {
-            if (sb.charAt(i) == '9') {
+        for (int i = 0; i < sb.length() - 1 && k > 0; i++) {
+            if (sb.charAt(i) == MAX_DIGIT) {
                 idx = i;
             }
             if (sb.charAt(i) < sb.charAt(i + 1)) {
