fc54e83e2fc799fea893f1ef7ec737e684b69242
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2023-12-10 13:33:24 +0900

    [level 2] Title: 숫자 변환하기, Time: 21.32 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    static final int MAX_NUMBER = 1_000_000;
+    static int[] dp;
+
+    public int solution(int x, int y, int n) {
+
+        dp = new int[MAX_NUMBER + 1];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = -1;
+        }
+        dp[x] = 0;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(x);
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            for (int next : new int[]{cur + n, cur * 2, cur * 3}) {
+                if (next > y) continue;
+                if (dp[next]  != -1) continue;
+                dp[next] = dp[cur] + 1;
+                q.offer(next);
+            }
+        }
+        return dp[y];
+    }
+}
