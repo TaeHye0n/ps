080abb0158c565de3b2523f436e9ca5aba1d9138
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2024-03-12 14:34:55 +0900

    [level 2] Title: 피보나치 수, Time: 0.54 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+#define MOD 1234567
+using namespace std;
+
+int dp[100001];
+
+int solution(int n) {
+    int answer = 0;
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        dp[i] = (dp[i-1] + dp[i-2])%MOD;
+    }
+    return dp[n];
+}
