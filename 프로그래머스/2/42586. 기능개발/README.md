ae8872c635a20b261330495281162a52c3978afe
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2024-03-22 20:31:52 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.03 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=cpp) 
 
 ### 성능 요약
 
-메모리: 72.8 MB, 시간: 2.62 ms
+메모리: 4.03 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 3일 14:25:59
+2024년 03월 22일 20:31:52
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+queue<int> q;
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    
+    for (int i = 0; i < speeds.size(); i++) {
+        q.push(i);
+    }
+    
+    while (!q.empty()) {
+        int cnt = 0;
+        for (int i = 0; i < speeds.size(); i++) {
+            progresses[i] += speeds[i];
+        }
+        
+        while (!q.empty() && progresses[q.front()] >= 100) {
+            cnt++;
+            q.pop();
+        }
+        
+        if (cnt > 0) answer.push_back(cnt);
+    }
+    return answer;
+}
