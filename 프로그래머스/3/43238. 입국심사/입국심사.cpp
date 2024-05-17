b382bd9479d70191342100178ea42b06e60d5aff
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2024-05-18 01:21:22 +0900

    [level 3] Title: 입국심사, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+
+ll solution(int n, vector<int> times) {
+    ll answer = 0;
+    sort(times.begin(), times.end());
+    
+    ll l = 1;
+    ll r = (ll) times.back() * n;
+    
+    while (l <= r) {
+        ll mid = (l + r) / 2;
+        ll cnt = 0;
+        
+        for (int i = 0; i < times.size(); i++) {
+            cnt += mid / times[i];
+            if (cnt >= n) break;
+        }
+        
+        if (cnt >= n) r = mid - 1;
+        else {
+            l = mid + 1;
+        }
+    }
+    return l;
+}
