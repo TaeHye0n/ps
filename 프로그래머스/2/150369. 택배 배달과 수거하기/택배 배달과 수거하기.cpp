126cab1df5d16c9760da1dca14c286ae9363f02b
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2024-09-29 17:21:06 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 2.36 ms, Memory: 9.58 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
@@ -2,52 +2,20 @@
 #include <vector>
 #include <iostream>
 using namespace std;
+typedef long long ll;
 
-long long solution(int cap, int n, vector<int> deliveries, vector<int> pickups) {
-    long long answer = 0;
-    int deliveryPoint = n - 1;
-    int pickupPoint = n - 1;
-    int goDist = -1, comeDist = -1;
-    bool flag = true;
-    while(flag){
-        int carry = cap;
-        for(int i = deliveryPoint; i >= 0; i--){
-            if(carry >= deliveries[i]){
-                if(deliveries[i] != 0 && goDist < i) goDist = i;
-                carry -= deliveries[i];
-                deliveries[i] = 0;
-                deliveryPoint = i;
-            }
-            else if(carry < deliveries[i]){
-                deliveryPoint = i;
-                deliveries[i] -= carry;
-                carry = 0;
-                if(goDist < i) goDist = i;
-                break;
-            }
+ll solution(int cap, int n, vector<int> deliveries, vector<int> pickups) {
+    ll answer = 0;
+    
+    ll deliver = 0, pickup = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        deliver += deliveries[i];
+        pickup += pickups[i];
+        while (deliver > 0 || pickup > 0) {
+            answer += (i + 1) * 2;
+            deliver -= cap;
+            pickup -= cap;
         }
-        
-        carry = cap;
-        for(int i = pickupPoint; i >= 0; i--){
-            if(carry >= pickups[i]){
-                if(pickups[i] != 0 && comeDist < i) comeDist = i;
-                carry -= pickups[i];
-                pickups[i] = 0;
-                pickupPoint = i;
-            }
-            else if(carry < pickups[i]){
-                pickups[i] -= carry;
-                carry = 0;
-                pickupPoint = i;
-                if(comeDist < i) comeDist = i;
-                break;
-            }
-        }
-        // cout << goDist << " " << comeDist << " " << deliveries[0] << endl;
-        
-        answer = answer + (long long)max(comeDist+1,goDist+1)*2;
-        comeDist = -1; goDist = -1;
-        if(deliveryPoint == 0 && pickupPoint == 0 && deliveries[0] ==0 && pickups[0] ==0) flag = false;
     }
     return answer;
 }
