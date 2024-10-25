e83d3d53172951a4c589b8750efffe02d1c82b7f
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2024-10-25 21:58:05 +0900

    [Gold IV] Title: 전화번호 목록, Time: 56 ms, Memory: 3176 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
@@ -1,43 +1,55 @@
 #include <iostream>
-#include<string>
-#include<vector>
-#include<algorithm>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+#include <climits>
+#include <queue>
+#include <unordered_map>
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define endl "\n"
+#define Y first
+#define X second
+
 using namespace std;
+typedef long long ll;
 
-vector<string> v;
-int n;
-
-void input() {
-	cin >> n;
-	for (int j = 0; j < n; j++) {
-		string temp = "";
-		cin >> temp;
-		v.push_back(temp);
-	}
-}
 
 int main() {
-   
+	FAST_IO;
 	int t = 0;
 	cin >> t;
-	for (int i = 1; i <= t; i++) {
+
+	while (t-- > 0) {
+		int n = 0;
+		cin >> n;
+		vector<string> input;
+		for (int i = 0; i < n; i++) {
+			string str;
+			cin >> str;
+			input.push_back(str);
+		}
+		
+		sort(input.begin(), input.end());
+
+
 		bool flag = true;
-		input();
-		sort(v.begin(), v.end());
+		for (int i = 0; i < input.size() - 1; i++) {
+			int len = input[i].size();
 
-		for (int j = 0; j < v.size() - 1; j++) {
-			int length = v[j].length();
-			int length2 = v[j + 1].length();
-
-			if (length > length2) continue;
-			if (v[j + 1].substr(0, length) == v[j]) {
+			if (input[i + 1].substr(0, len) == input[i]) {
 				flag = false;
 				break;
 			}
 		}
-		if (flag) cout << "YES" << "\n";
-		else cout << "NO" << "\n";
-		v.clear();
+
+
+		if (flag) {
+			cout << "YES" << endl;
+		}
+		else cout << "NO" << endl;
+
 	}
 	return 0;
 }
