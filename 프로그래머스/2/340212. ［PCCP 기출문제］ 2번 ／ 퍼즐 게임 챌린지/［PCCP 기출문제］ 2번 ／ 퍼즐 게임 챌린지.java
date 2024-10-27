580307ccce5723e2951bf35749afea24b7c7ce47
TaeHye0n <74584445+TaeHye0n@users.noreply.github.com>
2024-10-27 14:38:39 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 40.72 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -2,45 +2,38 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    
-    static int[] diffs;
-    static int[] times;
-    static long limit;
-    static int ans = Integer.MAX_VALUE;
-    static int n;
-    public int solution(int[] diffs2, int[] times2, long limit2) {
-        diffs = diffs2; times = times2; limit = limit2;
-        n = diffs.length;
+    public int solution(int[] diffs, int[] times, long limit) {
+        int answer = 100001;
+        
         int l = 1;
         int r = 100000;
         
         while (l <= r) {
             int mid = (l + r) / 2;
-            if (check(mid)) {
-                ans = Math.min(ans, mid);
+            if (isValid(diffs, times, limit, mid)) {
+                answer = Math.min(answer, mid);
                 r = mid - 1;
             } else {
                 l = mid + 1;
             }
         }
-        return ans;
+        return answer;
     }
     
-    static boolean check(int mid) {
-        long time = times[0];
+    boolean isValid(int[] diffs, int[] times, long limit, int level) {
+        long amount = times[0];
         
-        for (int i = 1; i < n; i++) {
-            if (mid >= diffs[i]) {
-                time += times[i];
+        for (int i = 1; i < diffs.length; i++) {
+            if (diffs[i] <= level) {
+                amount += times[i];
             } else {
-                long diff = diffs[i] - mid;
-                long timeSum = times[i] + times[i-1];
-                time += (diff * timeSum);
-                time += times[i];
+                long timeSum = times[i] + times[i - 1];
+                long level_diff = diffs[i] - level;
+                amount += level_diff * timeSum;
+                amount += times[i];
             }
+            if (amount > limit) return false;
         }
-        
-        if (time > limit) return false;
         return true;
     }
 }
